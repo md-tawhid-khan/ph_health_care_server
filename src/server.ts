30b062dd2333f47b2f7db7bf295fb4a75b4cfdac
md1300 <md.tawhid.khan1998@gmail.com>
2025-11-13 00:29:31 +0600

    use prisma logging and uncaughtException and unhandledRejectionError

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,28 @@ async function main() {
    const server:Server = app.listen(port,()=>{
       console.log('app is listening on port :',port);
    });
+
+   const exitHandler = async()=>{
+   if(server){
+      server.close(()=>{
+         console.log('server closed') ;
+      })
+   }
+} ;
+
+process.on('uncaughtException',(error)=>{
+   console.log(error) ;
+   exitHandler() ;
+}) ;
+
+process.on('unhandledRejection',(error)=>{
+   console.log(error);
+   exitHandler() ;
+})
+
 }
+
+
+
 
 main()
